d2bd1174e091f3cf19372abf82fcddcb408f3cbc
Yuanchu <yuanchu.dang@gmail.com>
2021-04-24 16:02:32 +0800

    Function doc

--- embeddings/utils.py
+++ embeddings/utils.py
@@ -6,6 +6,11 @@ from sentence_transformers import SentenceTransformer
 
 
 def get_pretrained_embeddings(model_name, sentences):
+    """
+    :param model_name: string representing the pretrained model
+    :param sentences: a list of strings (sentences)
+    :return: a list of sentence embeddings (equal length)
+    """
     model = SentenceTransformer(model_name)
     embeddings = model.encode(sentences)
     return embeddings
