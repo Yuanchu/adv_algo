927fc0c214a93bcc2ce48b2549082dfa2db68888
Yuanchu <yuanchu.dang@gmail.com>
2021-04-24 16:24:15 +0800

    Clean up the word embedding torch training into a function..

--- demo.py
+++ demo.py
@@ -2,9 +2,13 @@
 @author: Yuanchu Dang
 """
 
-from embeddings.utils import get_pretrained_embeddings
+from embeddings.utils import get_pretrained_embeddings, train_word_embeddings
 
 if __name__ == "main":
     model_name = 'paraphrase-distilroberta-base-v1'
-    sentences = ['This ', 'The quick brown fox jumps over the lazy dog.']
-    embeddings = get_pretrained_embeddings(model_name, sentences)
+    sentences = ['This ', 'This', 'The quick brown fox jumps over the lazy dog.']
+    embeddings1 = get_pretrained_embeddings(model_name, sentences)
+
+    sentence = 'hello hello hello world, what is going on'
+    embedding_size = 10
+    vocab, embeddings2 = train_word_embeddings(sentence, embedding_size)
