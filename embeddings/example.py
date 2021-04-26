89a0d263d4780b17dd75155d1f1a5e66295b3761
Yuanchu <yuanchu.dang@gmail.com>
2021-04-27 00:25:25 +0800

    Embedding refactoring

--- embeddings/example.py
+++ embeddings/example.py
@@ -1,12 +1,14 @@
 """
 @author: Yuanchu Dang
+
+Example script illustrating how to use the embedding utils.
 """
 
 from embeddings.utils import get_pretrained_embeddings, train_word_embeddings
 
 if __name__ == "main":
     model_name = 'paraphrase-distilroberta-base-v1'
-    sentences = ['This ', 'This', 'The quick brown fox jumps over the lazy dog.']
+    sentences = ['This ', 'This', 'That is fantastic.']
     embeddings1 = get_pretrained_embeddings(model_name, sentences)
 
     sentence = 'hello hello hello world, what is going on'
